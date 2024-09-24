5866a05c13ae621c974f51ee8be90c2b33c275bc
J. Vermeltfoort <s3787494@u0065075.vuw.leidenuniv.nl>
2024-09-24 17:23:24 +0200

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,89 +1,113 @@
 
 
-#include <fstream>
 #include <iostream>
-#include <iomanip>
+#include <fstream>
 
+#define LETTERS_SIZE 3
+#define EXPECTED_QUANTITY_ARGUMENTS 6
 
-int print_help(void)
+typedef enum ERRNO {
+    ERRNO_OK = 0,
+    ERRNO_ERR,
+} errno_t;
+
+std::fstream fs_input_file;
+std::fstream fs_output_file;
+char input_letters[LETTERS_SIZE] = {0};
+
+void cli_print_help(void){
+     std::cout << "-i <path> //path for input file." << std::endl;
+     std::cout << "-o <path> //path for output file." << std::endl;
+     std::cout << "-l <letters> //search for letters in input file, exactly 3 letters, no spaces, only letters." << std::endl;
+};
+
+errno_t parser_open_file_stream(std::fstream *fs, char* buf)
 {
-     std::cout << "Help:\t-c <file_path_in> <file_path_out> for encode." << std::endl;
-     std::cout << "\t-d <file_path_in> <file_path_out> for decode." << std::endl;
-     return 1;
-}
-
-int main (int argc, char *argv[]) {
-     std::fstream fs_in;
-     std::ofstream fs_out;
-     char c_prev = 0;
-     char c_now = 0;
-     int counter = 0;
-     int fs_in_char_count = 1;
-     int fs_out_char_count = 0;
-
-     if (argc == 1)
+     fs->open(buf);
+     
+     if (!fs->is_open())
      {
-          return print_help();
-     }
-     else
-     {
-          switch(argv[1][1]) {
-          case 'c':
-               std::cout << "c";
-               fs_in.open(argv[2]);
-               fs_out.open(argv[3]);
-               break;
-          case 'd':
-               std::cout << argv[3];
-               fs_in.open(argv[2]);
-               fs_out.open(argv[3]);
-               break;
-          default:
-               return print_help();
-          }
+          std::cout << "Failed to open file, please validate the path. File: " << buf << std::endl;
+               return ERRNO_ERR;
      }
 
+     return ERRNO_OK;
+};
 
-     c_prev = fs_in.get();
-     c_now = c_prev - 1;
+errno_t parser_letters(char* buf)
+{
+     uint8_t i = 0;
 
      do {
-          c_now = fs_in.get();
-          fs_in_char_count++;
-          if (c_now == c_prev)
-          {
-               counter++;
+          if (i >= LETTERS_SIZE) {
+               std::cout << "Too many letters provided." << std::endl;
+               return ERRNO_ERR;
           }
-          else 
+
+          input_letters[i] = buf[i] | 0x20;
+
+          if (input_letters[i] < 97 || input_letters[i] > 122)
           {
-               if (c_prev == '\\')
-               {
-                    fs_out << '\\';
-                    fs_out_char_count++;
-               }
-               else if (((c_prev >> 4) == 0x3) && ((c_prev & 0xF) <= 9))
-               {
-                    fs_out << '\\';
-                    fs_out_char_count++;
-               }
-               fs_out << c_prev;
-               fs_out_char_count++;
-               c_prev = c_now;
-               if (counter > 0)
-               {
-                    fs_out << counter + 1;
-                    fs_out_char_count++;
-                    counter = 0;
-               }
-               
+               std::cout << "Only letters may be provided. Provided was: " << input_letters[i] << std::endl;
+               return ERRNO_ERR;
           }
-     } while (!fs_in.eof());
 
-     std::cout << "Conversion rate: ";
-     std::cout << std::fixed << std::setprecision(2) << static_cast<float>(fs_out_char_count) / static_cast<float>(fs_in_char_count) * 100.0 << std::endl;
+          i++;
+     } while (buf[i] != '\0');
 
-     fs_in.close();
-     fs_out.close();
+     if (i != LETTERS_SIZE)
+     {
+          std::cout << "Not enough letters provided." << std::endl;
+     }
 
-     return 0;
+     return ERRNO_OK;
+}
+
+int main(int argc, char* argv[]) {
+    uint8_t i = 0;
+    uint8_t arg_init = 0X7;
+    errno_t rt_val = ERRNO_OK;
+
+    if (argc != (EXPECTED_QUANTITY_ARGUMENTS + 1)) {
+          cli_print_help();
+          std::cout << "Unexpected arguments, or not all required arguments provided. Args: " << argc << std::endl;
+          return ERRNO_ERR;
+     }
+
+    for (i = 1; i < argc ; i+=2)
+    {
+          switch (argv[i][1])
+          {
+               case 'i':
+                    rt_val = parser_open_file_stream(&fs_input_file, argv[i+1]);
+                    arg_init &= false << 0;
+                    break;
+               case 'o':
+                    rt_val = parser_open_file_stream(&fs_output_file, argv[i+1]);
+                    arg_init &= false << 1;
+                    break;
+               case 'l':
+                    rt_val = parser_letters(argv[i+1]);
+                    arg_init &= false << 2;
+                    break;
+               default:
+                    cli_print_help();
+                    std::cout << "Invalid argument provided. Argument: " << argv[i] << std::endl;
+                    return ERRNO_ERR;
+          }
+
+          if (rt_val != ERRNO_OK)
+          {
+               return rt_val;
+          }
+     }
+
+     if (arg_init != 0){
+          std::cout << "Invalid arguments provided, likely duplicates." << std::endl;
+          return ERRNO_ERR;
+
+     }
+     
+
+     return ERRNO_OK;
 }
