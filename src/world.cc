bb06929ed4b560eef94dbda08e77dff3f05c34c7
Jenny Vermeltfoort <jennyvermeltfoort@outlook.com>
2024-11-16 17:11:06 +0100

    fixed this shit lets go

--- src/world.cc
+++ src/world.cc
@@ -19,10 +19,10 @@ bool CellBoard::is_not_south_west_corner(const unsigned int x,
 }
 
 void CellBoard::populate_cell_east(cell_t* cell) {
-     if (cell->north != nullptr) {
+    if (cell->north != nullptr) {
         cell->north_east = cell->north->east;
     }
-      if (cell->south != nullptr) {
+    if (cell->south != nullptr) {
         cell->south_east = cell->south->east;
     }
 }
@@ -30,15 +30,21 @@ void CellBoard::populate_cell_east(cell_t* cell) {
 void CellBoard::recursive_populate_row_all(cell_t* cell) {
     cell_t* cell_west = cell->west;
 
-    if (cell_west->north =! nullptr) {
+    if (cell_west->north != nullptr) {
        cell->north_west = cell_west->north;
        cell->north = cell_west->north->east;
+    }
+
+    if (cell->east != nullptr && cell_west->north != nullptr) {
        cell->north_east = cell_west->north->east->east;
     }
     
     if (cell_west->south != nullptr) {
         cell->south_west = cell_west->south;
-        cell->south = cell_west->south->west;
+        cell->south = cell_west->south->east;
+    }
+
+    if (cell->east != nullptr && cell_west->south != nullptr) {
         cell->south_east = cell_west->south->east->east;
     }
 
@@ -48,32 +54,35 @@ void CellBoard::recursive_populate_row_all(cell_t* cell) {
 }
 
 cell_t* CellBoard::init_grid(void) {
-    cell_t* cell_y = new cell_t;
-    unsigned int y = board_size_y;
+    cell_t* cell_first = new cell_t;
+    cell_t *cell_y = cell_first;
 
-    while (y-- > 0) {
-        cell_t* cell_x = cell_y;
-        unsigned int x = board_size_x;
-        while (x-- > 0) {
-             cell_x->east = new cell_t;
-             cell_x->east->west = cell_x;
-             cell_x = cell_x->east;
-
-        }
+    for (int i = board_size_y - 1; i > 0 ; i--) {
         cell_y->south = new cell_t;
         cell_y->south->north = cell_y;
         cell_y = cell_y->south;
     }
 
-    cell_t* cell_first = cell_y;
-    while(y++ < board_size_y) {
-        populate_cell_east(cell_first);
-        recursive_populate_row_all(cell_first->east);
-        cell_first = cell_first->north;
-            
+
+    cell_y = cell_first;
+    while (cell_y != nullptr) {
+        cell_t* cell_x = cell_y;
+	for (int i = board_size_x - 1 ; i > 0 ; i--) {
+             cell_x->east = new cell_t;
+             cell_x->east->west = cell_x;
+             cell_x = cell_x->east;
+        }
+	cell_y = cell_y->south;
     }
 
-    return cell_y; // most north western cell
+    cell_y = cell_first;
+    while(cell_y != nullptr) {
+        populate_cell_east(cell_y);
+        recursive_populate_row_all(cell_y->east);
+	cell_y = cell_y->south;
+    }
+    
+    return cell_first; // most north western cell
 }
 
 int cell_count(cell_t* cell) {
@@ -84,13 +93,16 @@ int cell_count(cell_t* cell) {
 }
 
 void CellBoard::print(void) {
-    cell_t* cell_x;
+    cell_t* cell_x =board_start;
     cell_t* cell_y = board_start;
+    int i = 0;
 
-    while (cell_y->south != nullptr) {
+    while (cell_y != nullptr) {
         cell_x = cell_y;
 
-        while (cell_x->east != nullptr) {
+	std::cout << +i++  << ":";
+
+        while (cell_x != nullptr) {
             std::cout << cell_count(cell_x);
             // if (cell_x->is_bomb) {
             //     std::cout << "x";
@@ -129,19 +141,19 @@ cell_t* CellBoard::grid_walk_south_east(cell_t* cell,
     return cell;
 }
 
-cell_t* CellBoard::grid_get_cell(unsigned int x, unsigned int y) {
-    cell_t* cell = nullptr;
+cell_info_t* CellBoard::grid_get_cell_info(unsigned int x, unsigned int y) {
+    cell_t* cell;
     unsigned int steps_diagonal;
 
     if (x > board_size_x || y > board_size_y) {
-        return cell;
+        return nullptr;
     }
 
     steps_diagonal = min(x, y);
     cell = grid_walk_south_east(board_start, steps_diagonal);
     cell = grid_walk_east(cell, x - steps_diagonal);
     cell = grid_walk_south(cell, y - steps_diagonal);
-    return cell;
+    return &cell->info;
 }
 
 /**
--- src/world.h
+++ src/world.h
@@ -26,24 +26,17 @@ class CellBoard {
     cell_t *board_start;
     const unsigned int board_size_x;
     const unsigned int board_size_y;
-    cell_t *init_cell_all_east(cell_t *cell, const unsigned int x,
-                               const unsigned int y);
-    cell_t *init_cell_all_south(cell_t *cell, const unsigned int x,
-                                const unsigned int y);
-    cell_t *init_raster(void);
-    void populate_all(cell_t *cell);
-    void populate_all_east(cell_t *cell);
-    void populate_south(cell_t *cell);
-    void populate_north_row(cell_t *cell);
-    void populate_west_column(cell_t *cell);
+    cell_t *init_grid(void);
+    void populate_cell_east(cell_t *cell);
+    void recursive_populate_row_all(cell_t *cell);
     bool is_not_south_edge(const unsigned int y);
     bool is_not_east_edge(const unsigned int x);
     bool is_not_south_west_corner(const unsigned int x,
                                   const unsigned int y);
-    cell_t *raster_walk_east(cell_t *cell, unsigned int steps);
-    cell_t *raster_walk_south(cell_t *cell, unsigned int steps);
-    cell_t *raster_walk_south_east(cell_t *cell, unsigned int steps);
-    cell_info_t *raster_get_cell_info(unsigned int x, unsigned int y);
+    cell_t *grid_walk_east(cell_t *cell, unsigned int steps);
+    cell_t *grid_walk_south(cell_t *cell, unsigned int steps);
+    cell_t *grid_walk_south_east(cell_t *cell, unsigned int steps);
+    cell_info_t *grid_get_cell_info(unsigned int x, unsigned int y);
 
    public:
     CellBoard(const unsigned int size_x, const unsigned int size_y,
